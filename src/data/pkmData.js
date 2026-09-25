11560c056c547e396f5f6b2aadde273027b59b27
farizqii <Farizqi122@gmail.com>
2026-09-25 13:42:33 +0700

    Added PKM Data

---
+++ src/data/pkmData.js
@@ -0,0 +1,42 @@
+export const pkmData = [
+  {
+    Name: "PKM-RE",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-RSH",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-PI",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-PM",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-KI",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-KC",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-K",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+  {
+    Name: "PKM-VGK",
+    Description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+  },
+];
