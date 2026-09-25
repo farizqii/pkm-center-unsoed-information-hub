814884696701844b962abfb644e083724204a245
farizqii <Farizqi122@gmail.com>
2026-09-25 17:08:15 +0700

    Feat: Updated PKM Brief Descriptions

--- src/data/pkmData.js
+++ src/data/pkmData.js
@@ -2,41 +2,41 @@ export const pkmData = [
   {
     Name: "PKM-RE",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Riset Eksakta: Program penelitian yang berfokus pada pengungkapan hubungan sebab-akibat, fenomena ilmiah, serta pengujian produk/teknologi berbasis sains dan eksakta.",
   },
   {
     Name: "PKM-RSH",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Riset Sosial Humaniora: Program penelitian yang mengeksplorasi fenomena sosial, perilaku masyarakat, humaniora, seni, dan budaya untuk memberikan solusi atas permasalahan sosial.",
   },
   {
     Name: "PKM-PI",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Penerapan IPTEK: Program bantuan solusi teknologi atau iptek berbasis kebutuhan nyata bagi mitra kerja berskala produktif/komersial (seperti UMKM atau industri kecil).",
   },
   {
     Name: "PKM-PM",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Pengabdian Kepada Masyarakat: Program penerapan sains dan teknologi untuk membantu menyelesaikan masalah yang dihadapi oleh mitra non-profit (seperti masyarakat umum, karang taruna, atau panti asuhan).",
   },
   {
     Name: "PKM-KI",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Karya Inovatif: Program pembuatan produk inovatif berbasis teknologi yang berpotensi memiliki nilai komersial, berdaya saing tinggi, dan siap diproduksi dalam skala terbatas.",
   },
   {
     Name: "PKM-KC",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Karsa Cipta: Program penciptaan karya berbasis karsa dan nalar mahasiswa yang menghasilkan sistem, alat, atau prototipe yang siap diuji coba secara teknis.",
   },
   {
     Name: "PKM-K",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Kewirausahaan: Program pengembangan bisnis berbasis produk berupa barang atau jasa yang inovatif, berorientasi pada profit, dan memiliki analisis pasar yang matang.",
   },
   {
     Name: "PKM-VGK",
     Description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut augue sapien, mollis vel arcu id, efficitur eleifend urna. Cras sed imperdiet velit. Nullam eu lacinia urna. Curabitur scelerisque tellus dignissim, ultricies lacus nec, auctor purus.",
+      "PKM Video Gagasan Konstruktif: Program penyampaian isu-isu strategis bangsa beserta solusi futuristik dan imajinatif yang dikemas dalam bentuk media video naratif yang menarik.",
   },
 ];
