16e94dff4077d92bf7cf8bb645c9ece7565d8e22
farizqii <Farizqi122@gmail.com>
2026-09-26 09:05:50 +0700

    style: Removed Hero Section hovering effect

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -25,11 +25,11 @@ export default function HeroSection() {
         />
       </div>
       <div className="flex w-full max-w-7xl flex-col items-center text-center">
-        <h1 className="text-xl font-normal text-white sm:text-2xl md:text-2xl lg:text-3xl hover:scale-105 transition-transform ease-in-out">
+        <h1 className="text-xl font-normal text-white sm:text-2xl md:text-2xl lg:text-3xl">
           Selamat Datang di
         </h1>
 
-        <h2 className="mt-3 flex flex-col items-center text-center font-black italic tracking-tight text-3xl sm:text-4xl md:text-5xl lg:text-[3.25rem] xl:text-[4rem] leading-tight hover:scale-105 transition-transform ease-in-out">
+        <h2 className="mt-3 flex flex-col items-center text-center font-black italic tracking-tight text-3xl sm:text-4xl md:text-5xl lg:text-[3.25rem] xl:text-[4rem] leading-tight">
           <span className="bg-linear-to-r from-[#EEAB01] via-[#FFFFFF] to-[#EEAB01] bg-clip-text text-transparent lg:whitespace-nowrap">
             Program Kreativitas Mahasiswa Center Unsoed
           </span>
